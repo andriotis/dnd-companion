678fb2f49b9be163951d71d28d2f3c814e455879
andriotis <andriotis@ece.auth.gr>
2023-09-24 17:57:40 +0300

    create unstyled dice roller with reset

--- src/components/roll/Roll.jsx
+++ src/components/roll/Roll.jsx
@@ -1,69 +1,85 @@
-import { D4, D6, D8, D10, D12, D20 } from "../../assets/roll/Die";
+import { PlusIcon, MinusIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
-const die = [
-  { value: 2, icon: <D4 /> },
-  { value: 4, icon: <D6 /> },
-  { value: 6, icon: <D8 /> },
-  { value: 8, icon: <D10 /> },
-  { value: 10, icon: <D12 /> },
-  { value: 12, icon: <D20 /> },
-];
-
-const DiceCard = ({ icon }) => {
-  const [amount, setAmount] = useState(0);
+const DiceCard = ({ value, onDecrementHandler, onIncrementHandler }) => {
   return (
     <div className="flex">
-      <div>{icon}</div>
-      <div
-        onClick={() => {
-          setAmount(amount + 1);
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <div>d{value}</div>
+      <div onClick={onIncrementHandler}>
+        <PlusIcon className="h-6 w-6 text-gray-950" />
       </div>
-      <div
-        onClick={() => {
-          setAmount(amount - 1);
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3.75 12a.75.75 0 01.75-.75h15a.75.75 0 010 1.5h-15a.75.75 0 01-.75-.75z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <div onClick={onDecrementHandler}>
+        <MinusIcon className="h-6 w-6 text-gray-950" />
       </div>
-      <div>{amount}</div>
     </div>
   );
 };
 
 export default function Roll() {
   const [result, setResult] = useState(0);
+  const [writtenProof, setWrittenProof] = useState("");
+
+  const [die, setDie] = useState([
+    [4, 0],
+    [6, 0],
+    [8, 0],
+    [10, 0],
+    [12, 0],
+    [20, 0],
+  ]);
+
+  function onIncrementHandler(index) {
+    const newDie = [...die];
+    newDie[index][1]++;
+    setDie(newDie);
+  }
+
+  function onDecrementHandler(index) {
+    const newDie = [...die];
+    newDie[index][1]--;
+    setDie(newDie);
+  }
+
+  function onRollClickHandler() {
+    let total = 0;
+    die.forEach((dice) => {
+      for (let i = 0; i < dice[1]; i++) {
+        let curr_roll = Math.floor(Math.random() * dice[0]) + 1;
+        setWrittenProof((prev) => prev + `d${dice[0]}: ${curr_roll}\n`);
+        total += curr_roll;
+      }
+    });
+    setResult(total);
+  }
+
+  function onResetClickHandler() {
+    setResult(0);
+    setDie([
+      [4, 0],
+      [6, 0],
+      [8, 0],
+      [10, 0],
+      [12, 0],
+      [20, 0],
+    ]);
+  }
+
   return (
     <div>
-      {die.map((dice) => (
-        <DiceCard key={dice.value} {...dice} />
-      ))}
-      <div>{result}</div>
+      <div className="flex flex-col">
+        {die.map((die, index) => (
+          <DiceCard
+            key={index}
+            value={die[0]}
+            onDecrementHandler={() => onDecrementHandler(index)}
+            onIncrementHandler={() => onIncrementHandler(index)}
+          />
+        ))}
+      </div>
+      <button onClick={onRollClickHandler}>
+        Roll: {result} {writtenProof}
+      </button>
+      <button onClick={onResetClickHandler}>Reset</button>
     </div>
   );
 }
